8804e60d22af53e881ef03bdbb3afbec8716876f
JialiSupergirl <101123963+JialiSupergirl@users.noreply.github.com>
2022-03-21 15:02:48 -0400

    Delete db.py

--- db.py
+++
@@ -1,9 +0,0 @@
-import mariadb
-
-def getConnection():
-    dbConn = mariadb.connect( host="localhost", user="root", password="", database= "w11")
-    cur = dbConn.cursor()
-    return cur
-
-def closeConn(cur):
-    cur.close()
